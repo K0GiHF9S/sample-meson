25f4fb11a1286cd2675f5255c897e2b18d98289a
K0GiHF9S <kidoairakusyunkasyuutou@gmail.com>
2024-12-07 08:16:01 +0900

    a

--- tests/commonTest.h
+++ tests/commonTest.h
@@ -29,18 +29,11 @@ private:
 template <typename T>
 T *GMockHelper<T>::instance = nullptr;
 
-#define MOCK_FUNCTION_IMPL(_ClassName, _FunctionName, _N, _Signature)                            \
-    typename ::testing::internal::Function<GMOCK_PP_REMOVE_PARENS(_Signature)>::Result           \
-    _FunctionName(GMOCK_PP_REPEAT(GMOCK_INTERNAL_PARAMETER, _Signature, _N))                     \
-    {                                                                                            \
-        _ClassName *mock = _ClassName::GetInstance();                                            \
-        if (mock == nullptr)                                                                     \
-        {                                                                                        \
-            std::stringstream msg;                                                               \
-            msg << "Error: Mock " << #_ClassName << "  is not instantiated.";                    \
-            throw std::logic_error(msg.str());                                                   \
-        }                                                                                        \
-        return mock->_FunctionName(GMOCK_PP_REPEAT(GMOCK_INTERNAL_FORWARD_ARG, _Signature, _N)); \
+#define MOCK_FUNCTION_IMPL(_ClassName, _FunctionName, _N, _Signature)                                                 \
+    typename ::testing::internal::Function<GMOCK_PP_REMOVE_PARENS(_Signature)>::Result                                \
+    _FunctionName(GMOCK_PP_REPEAT(GMOCK_INTERNAL_PARAMETER, _Signature, _N))                                          \
+    {                                                                                                                 \
+        return _ClassName::GetInstance()->_FunctionName(GMOCK_PP_REPEAT(GMOCK_INTERNAL_FORWARD_ARG, _Signature, _N)); \
     }
 
 #define MOCK_FUNCTION(_ClassName, _Ret, _FunctionName, _Args) \
